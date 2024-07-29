8101d124c26578c7daf5277d4347d1b95061ca7b
Hui9on <gmlrhs281@naver.com>
2024-07-29 15:02:32 +0900

    타이머가 끝나면 소리가 나도록 사운드 파일 추가, 경로 추가

--- src/components/TimerPage.jsx
+++ src/components/TimerPage.jsx
@@ -157,7 +157,7 @@ const TimerPage = () => {
         <Button onClick={resetTimer}>리셋</Button>
       </div>
       <audio ref={audioRef} preload="auto">
-        <source src="/path/to/your/alarm-sound.mp3" type="audio/mpeg" />
+        <source src="/audio/bip.mp3" type="audio/mpeg" />
         Your browser does not support the audio element.
       </audio>
     </TimerWrapper>
